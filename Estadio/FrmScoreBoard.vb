cdf8861cf5aa24a2ec412a1e93d88617ebb8c0f7
Daniel Medina <danielmedinamendez@gmail.com>
2017-01-24 08:36:24 +0100

    Finalizado el interface

--- Estadio/FrmScoreBoard.vb
+++ Estadio/FrmScoreBoard.vb
@@ -5,6 +5,8 @@
     Public Overrides Sub actualizar()
         Dim desk As frmDesk = CType(Me.sujeto, frmDesk)
         Me.MarcadorSinLogo.setHomeTeam(desk.tbHomeTeam.Text)
+        Me.MarcadorSinLogo.setHomeScore(desk.nudHome.Text)
+
 
     End Sub
 End Class
